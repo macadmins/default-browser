2a406df4ee0274eb1cc8fec4f16b58b26ae13227
Graham Gilbert <graham.gilbert@airbnb.com>
2025-02-19 21:17:41 -0800

    More output

--- tools/build_pkg.zsh
+++ tools/build_pkg.zsh
@@ -15,6 +15,8 @@ INSTALLER_SIGNING_IDENTITY="Developer ID Installer: Mac Admins Open Source (T4SK
 # read the version from passed argument
 VERSION=$1
 
+echo "Building version ${VERSION}"
+
 # build arm64
 GOOS=darwin GOARCH=arm64 go build -ldflags "-X main.version=${VERSION}" -o build/default-browser-arm64
 
@@ -22,13 +24,17 @@ GOOS=darwin GOARCH=arm64 go build -ldflags "-X main.version=${VERSION}" -o build
 GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=${VERSION}" -o build/default-browser-amd64
 
 # combine the two into a universal binary
+echo "Combining arm64 and amd64 into a universal binary"
 /usr/bin/lipo -create -output build/default-browser build/default-browser-arm64 build/default-browser-amd64
 
 # copy the binary to the payload
+echo "Copying the binary to the payload"
 /bin/cp build/default-browser payload/opt/macadmins/bin/default-browser
 
 # sign the binary
+echo "Signing the binary"
 sudo /usr/bin/codesign --timestamp --force --deep -s "${APP_SIGNING_IDENTITY}" payload/opt/macadmins/bin/default-browser
 
 # create the package
+echo "Creating the package"
 /usr/bin/pkgbuild --root payload --identifier com.github.macadmins.default-browser --version ${VERSION} --install-location / --ownership recommended --sign "${INSTALLER_SIGNING_IDENTITY}" output/default-browser.pkg
