586301db0a36b12080f7bff6fe88f7a26db36ec4
Graham Gilbert <graham.gilbert@airbnb.com>
2026-07-03 22:43:12 -0700

    Prepare 1.1 release

--- pkg/client/client_test.go
+++ pkg/client/client_test.go
@@ -15,56 +15,36 @@ func TestNewClient(t *testing.T) {
 	assert.NoError(t, err, "NewClient should not return an error")
 	assert.NotNil(t, client.Runner, "Runner should not be nil")
 	assert.IsType(t, &osq.ExecCmdRunner{}, client.Runner, "Runner should be of type osq.Runner")
-	assert.NotEmpty(t, client.CurrentUser, "CurrentUser should not be empty")
 	assert.NotEmpty(t, client.HomeDir, "HomeDir should not be empty")
 	assert.Equal(t, defaultLaunchServicesPlistLocation(client.HomeDir), client.PlistLocation, "PlistLocation should use the current user's actual home directory")
 }
 
-func TestNewClientWithCurrentUser(t *testing.T) {
-	expectedUser := "testuser"
-	expectedHomeDir := "/Volumes/test_volume/Users/systemuser"
-
-	client, err := newClient(
-		func() (*user.User, error) {
-			return &user.User{Username: "systemuser", HomeDir: expectedHomeDir}, nil
-		},
-		WithCurrentUser(expectedUser),
-	)
-
-	assert.NoError(t, err, "NewClient should not return an error")
-	assert.Equal(t, expectedUser, client.CurrentUser, "CurrentUser should be set to the provided value")
-	assert.Equal(t, expectedHomeDir, client.HomeDir, "HomeDir should be set from the current user lookup")
-	assert.Equal(t, defaultLaunchServicesPlistLocation(expectedHomeDir), client.PlistLocation, "PlistLocation should use the current user's actual home directory")
-}
-
 func TestNewClientWithHomeDir(t *testing.T) {
 	expectedHomeDir := "/Volumes/test_volume/Users/testuser"
+	expectedPlistLocation := "/Volumes/test_volume/Users/testuser/Library/Preferences/com.apple.LaunchServices/com.apple.launchservices.secure.plist"
 
 	client, err := newClient(
 		func() (*user.User, error) {
-			return &user.User{Username: "systemuser", HomeDir: "/Users/systemuser"}, nil
+			return nil, errors.New("current user lookup should not be called")
 		},
 		WithHomeDir(expectedHomeDir),
 	)
 
 	assert.NoError(t, err, "NewClient should not return an error")
-	assert.Equal(t, "systemuser", client.CurrentUser, "CurrentUser should be set from the current user lookup")
 	assert.Equal(t, expectedHomeDir, client.HomeDir, "HomeDir should be set to the provided value")
-	assert.Equal(t, defaultLaunchServicesPlistLocation(expectedHomeDir), client.PlistLocation, "PlistLocation should use the provided home directory")
+	assert.Equal(t, expectedPlistLocation, client.PlistLocation, "PlistLocation should use the provided home directory")
 }
 
-func TestNewClientDefaultCurrentUser(t *testing.T) {
-	expectedUser := "systemuser"
+func TestNewClientDefaultHomeDir(t *testing.T) {
 	expectedHomeDir := "/Volumes/test_volume/Users/systemuser"
 
 	client, err := newClient(
 		func() (*user.User, error) {
-			return &user.User{Username: expectedUser, HomeDir: expectedHomeDir}, nil
+			return &user.User{HomeDir: expectedHomeDir}, nil
 		},
 	)
 
 	assert.NoError(t, err, "NewClient should not return an error")
-	assert.Equal(t, expectedUser, client.CurrentUser, "CurrentUser should be set to the system's current user")
 	assert.Equal(t, expectedHomeDir, client.HomeDir, "HomeDir should be set to the system's current user's home directory")
 	assert.Equal(t, defaultLaunchServicesPlistLocation(expectedHomeDir), client.PlistLocation, "PlistLocation should use the current user's actual home directory")
 }
@@ -74,35 +54,17 @@ func TestNewClientWithPlistLocation(t *testing.T) {
 
 	client, err := newClient(
 		func() (*user.User, error) {
-			return &user.User{Username: "systemuser", HomeDir: "/Volumes/test_volume/Users/systemuser"}, nil
+			return nil, errors.New("current user lookup should not be called")
 		},
 		WithPlistLocation(expectedPlistLocation),
 	)
 
 	assert.NoError(t, err, "NewClient should not return an error")
+	assert.Empty(t, client.HomeDir, "HomeDir should not be set when only PlistLocation is provided")
 	assert.Equal(t, expectedPlistLocation, client.PlistLocation, "PlistLocation should be set to the provided value")
 }
 
-func TestNewClientWithCurrentUserAndHomeDirSkipsCurrentUserLookup(t *testing.T) {
-	expectedUser := "testuser"
-	expectedHomeDir := "/Volumes/test_volume/Users/testuser"
-
-	client, err := newClient(
-		func() (*user.User, error) {
-			return nil, errors.New("current user lookup should not be called")
-		},
-		WithCurrentUser(expectedUser),
-		WithHomeDir(expectedHomeDir),
-	)
-
-	assert.NoError(t, err, "NewClient should not return an error")
-	assert.Equal(t, expectedUser, client.CurrentUser, "CurrentUser should be set to the provided value")
-	assert.Equal(t, expectedHomeDir, client.HomeDir, "HomeDir should be set to the provided value")
-	assert.Equal(t, defaultLaunchServicesPlistLocation(expectedHomeDir), client.PlistLocation, "PlistLocation should use the provided home directory")
-}
-
-func TestNewClientWithCurrentUserHomeDirAndPlistLocationSkipsCurrentUserLookup(t *testing.T) {
-	expectedUser := "testuser"
+func TestNewClientWithHomeDirAndPlistLocation(t *testing.T) {
 	expectedHomeDir := "/Volumes/test_volume/Users/testuser"
 	expectedPlistLocation := "/tmp/test.plist"
 
@@ -110,13 +72,11 @@ func TestNewClientWithCurrentUserHomeDirAndPlistLocationSkipsCurrentUserLookup(t
 		func() (*user.User, error) {
 			return nil, errors.New("current user lookup should not be called")
 		},
-		WithCurrentUser(expectedUser),
 		WithHomeDir(expectedHomeDir),
 		WithPlistLocation(expectedPlistLocation),
 	)
 
 	assert.NoError(t, err, "NewClient should not return an error")
-	assert.Equal(t, expectedUser, client.CurrentUser, "CurrentUser should be set to the provided value")
 	assert.Equal(t, expectedHomeDir, client.HomeDir, "HomeDir should be set to the provided value")
 	assert.Equal(t, expectedPlistLocation, client.PlistLocation, "PlistLocation should be set to the provided value")
 }
@@ -131,7 +91,6 @@ func TestNewClientReturnsCurrentUserLookupError(t *testing.T) {
 	)
 
 	assert.ErrorIs(t, err, expectedErr, "NewClient should return the current user lookup error")
-	assert.Empty(t, client.CurrentUser, "CurrentUser should not be set when lookup fails")
 	assert.Empty(t, client.HomeDir, "HomeDir should not be set when lookup fails")
 	assert.Empty(t, client.PlistLocation, "PlistLocation should not be set when lookup fails")
 }
